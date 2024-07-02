43dc11bcf1390a5b96c126ee68a217e1781c5f24
ahsu <ahsu@g.harvard.edu>
2024-07-01 23:57:12 -0400

    Re-prompt the user to select their google account every time

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,12 @@ const Login = () => {
         const clientId = '55kpv1cuiekc2drftj2ftr71nu';
         const redirectUri = encodeURIComponent('https://optipri.me/idpresponse');
         // noinspection UnnecessaryLocalVariableJS
-        const loginUrl = `https://auth.optipri.me/oauth2/authorize?response_type=token&client_id=${clientId}&redirect_uri=${redirectUri}`;
+        const loginUrl = (`https://auth.optipri.me/oauth2/authorize?
+                           response_type=token&
+                           client_id=${clientId}&
+                           redirect_uri=${redirectUri}&
+                           prompt=select_account`
+                          .replace(/s+/g, ''));
         window.location.href = loginUrl;
     };
     return <GoogleButton onClick={initiateLogin} />;
